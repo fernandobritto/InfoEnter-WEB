a60c933b46bdde3a9125ea6c377677019b4b5e8c
Fernando Britto <fernandobritto.web@gmail.com>
2021-02-21 07:10:36 -0300

    Initial commit

---
+++ src/components/HomePage/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from './styles'
+import { PostData } from '../../domain/posts/post'
+import { Header } from '../../components/Header'
+
+export type HomePageProps = {
+  posts: PostData[]
+}
+
+export default function HomePage({ posts }: HomePageProps) {
+  return (
+    <>
+      <Header />
+      <Container>
+        {posts.map((post) => (
+          <h2 key={post.slug}>{post.title}</h2>
+        ))}
+      </Container>
+    </>
+  )
+}
